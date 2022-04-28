36bd4b2bc8bd8a1a8319745a770d7f8d0b69b74c
胡波 <1165054597@qq.com>
2022-04-28 10:29:14 +0800

    实现emit

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -8,3 +8,27 @@ export const hasChanged = (value, newValue) => !Object.is(value, newValue);
 
 export const hasOwn = (val, key) =>
   Object.prototype.hasOwnProperty.call(val, key);
+
+/**
+ * @name 转驼峰方法
+ * @param {String} str 
+ * @returns {String} res
+ */
+export const camelize = (str: string) => {
+  return str.replace(/-(\w)/g, (_, c: string) => {
+    return c ? c.toUpperCase() : "";
+  });
+};
+
+const capitalize = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+/**
+ * @name 处理on函数
+ * @param str 
+ * @returns {String} res
+ */
+export const toHandlerKey = (str: string) => {
+  return str ? "on" + capitalize(str) : "";
+};
